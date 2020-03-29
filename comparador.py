0511947f9d71957a6741697964be0470e5dec022
camylawojcik <camylaw89@gmail.com>
2020-03-29 15:01:30 -0300

    Update Comparador

--- comparador.py
+++ comparador.py
@@ -7,7 +7,7 @@ import os
 
 def load_database():
 
-    base_ativos = pd.read_csv('Ativos.csv', sep=';')
+    base_ativos = pd.read_csv('/app/Ativos.csv', sep=';')
 
     return base_ativos
 
@@ -21,8 +21,8 @@ if __name__ == '__main__':
 
     selic_rate, max_date = get_selic(base_ativos['vencimento'].min())
 
-    print(f'IRR Rate:   {round(rate,6) *100}%')
-    print(f'Taxa Selic {max_date}: {selic_rate}')
+    print(f'\n IRR Rate:   {round(rate,6) *100}%')
+    print(f'\n Taxa Selic {max_date}: {selic_rate}')
 
-    print(f"Projeção de Taxa Selic Acumulada entre {base_ativos['vencimento'].min()} "
-          f"- {base_ativos['vencimento'].max()}:   {selic_rate * base_ativos['posicao'].max()}%")
+    print(f"\n Projeção de Taxa Selic Acumulada entre {base_ativos['vencimento'].min()} "
+          f"- {base_ativos['vencimento'].max()}:   {selic_rate * base_ativos['posicao'].max()}%\n")
